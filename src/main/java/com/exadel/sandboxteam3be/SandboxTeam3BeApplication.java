49bb98859ace412cb29d5d28a29042c3858e3ea6
VolodymyrBushko <volodymyr.bushko@gmail.com>
2021-06-02 15:27:16 +0300

    Init

---
+++ src/main/java/com/exadel/sandboxteam3be/SandboxTeam3BeApplication.java
@@ -0,0 +1,12 @@
+package com.exadel.sandboxteam3be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxTeam3BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxTeam3BeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/exadel/sandboxteam3be/SandboxTeam3BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exadel.sandboxteam3be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxTeam3BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
