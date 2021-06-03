db9a59cbdf977502c37959a27c0f964632fa0494
dxdNvqKP <7VDO8j7kEg>
2021-06-03 20:21:35 +0300

    added UserDiscount entity

---
+++ src/main/java/com/exadel/discountwebapp/user/UserDiscount.java
@@ -0,0 +1,34 @@
+package com.exadel.discountwebapp.user;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Table(name = "user_discount")
+public class UserDiscount {
+
+    @ToString.Exclude
+    @ManyToOne(optional = true)
+    @JoinColumn(name = "user_id", nullable = true)
+    private User user;
+
+    @ToString.Exclude
+    @ManyToOne(optional = true)
+    @JoinColumn(name = "discount_id", nullable = true)
+    private Discount discount;
+
+    @Column(name = "discount_amount")
+    int discountAmount;
+
+    @Column(name = "created_at")
+    LocalDateTime createdAt;
+
+    @Column(name = "modified_at")
+    LocalDateTime modifiedAt;
+}
