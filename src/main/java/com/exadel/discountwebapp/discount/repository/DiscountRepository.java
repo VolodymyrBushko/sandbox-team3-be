0d6ec8f9e4ca70cac47444f7cbcf0874d34b7343
VolodymyrBushko <volodymyr.bushko@gmail.com>
2021-06-06 14:38:01 +0300

    Added @Repository annotation to repositories

--- src/main/java/com/exadel/discountwebapp/discount/repository/DiscountRepository.java
+++ src/main/java/com/exadel/discountwebapp/discount/repository/DiscountRepository.java
@@ -2,6 +2,8 @@ package com.exadel.discountwebapp.discount.repository;
 
 import com.exadel.discountwebapp.discount.entity.Discount;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DiscountRepository extends CrudRepository<Discount, Long> {
 }
