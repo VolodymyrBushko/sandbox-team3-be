3692a19d19bbff538ab7b7490054fc24e7163312
Bozhokmaria <bozhokmaria@gmail.com>
2021-06-06 14:35:56 +0300

    added @Repository

--- src/main/java/com/exadel/discountwebapp/role/repository/RoleRepository.java
+++ src/main/java/com/exadel/discountwebapp/role/repository/RoleRepository.java
@@ -2,7 +2,9 @@ package com.exadel.discountwebapp.role.repository;
 
 import com.exadel.discountwebapp.role.entity.Role;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends CrudRepository<Role, Long> {
 
 }
--- src/main/java/com/exadel/discountwebapp/user/repository/UserRepository.java
+++ src/main/java/com/exadel/discountwebapp/user/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.exadel.discountwebapp.user.repository;
 
 import com.exadel.discountwebapp.user.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
 }
