dc8c78d8a2f14915f985dd65174ee8d171416237
dxdNvqKP <7VDO8j7kEg>
2021-06-06 15:07:23 +0300

    replace Long to UserDiscount.UserDiscountId in interface UserDiscountRepository

--- src/main/java/com/exadel/discountwebapp/user/repository/UserDiscountRepository.java
+++ src/main/java/com/exadel/discountwebapp/user/repository/UserDiscountRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserDiscountRepository extends CrudRepository<UserDiscount, Long> {
+public interface UserDiscountRepository extends CrudRepository<UserDiscount, UserDiscount.UserDiscountId> {
 
 }
