1e5de7493981424428ec6df3c947398508ba0dfe
VolodymyrBushko <volodymyr.bushko@gmail.com>
2021-06-08 14:24:34 +0300

    Included SpringValidation. Included and set up ModelMapper

--- src/main/java/com/exadel/discountwebapp/DiscountWebApplication.java
+++ src/main/java/com/exadel/discountwebapp/DiscountWebApplication.java
@@ -1,7 +1,9 @@
 package com.exadel.discountwebapp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -10,5 +12,10 @@ public class DiscountWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DiscountWebApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
