f16b872c64aca7ab452e25926f26a6803f70987d
dxdNvqKP <7VDO8j7kEg>
2021-06-09 21:49:26 +0300

    deleted classes = DiscountWebApplication.class

--- src/test/java/com/exadel/discountwebapp/role/RoleServiceIntegrationTest.java
+++ src/test/java/com/exadel/discountwebapp/role/RoleServiceIntegrationTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts =
         "classpath:cleanup.sql")
-@SpringBootTest(classes = DiscountWebApplication.class)
+@SpringBootTest()
 public class RoleServiceIntegrationTest {
 
     @Autowired
