70ed0e7bce532350ecdbeeab6619bc686d17b507
VolodymyrBushko <volodymyr.bushko@gmail.com>
2021-06-10 13:59:08 +0300

    Added WebExceptionHandler and EntityNotFoundException

---
+++ src/main/java/com/exadel/discountwebapp/exception/WebExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.exadel.discountwebapp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+public class WebExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
+    public String methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        FieldError field = ex.getBindingResult().getFieldError();
+        return String.format("%s.%s.%s", field.getObjectName(), field.getField(), field.getDefaultMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public String constraintViolationException(ConstraintViolationException ex) {
+        String message = ex.getMessage();
+        int startSubstring = message.indexOf('.') + 1;
+        return message.substring(startSubstring);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public String entityNotFoundException(EntityNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public String globalException() {
+        return "Something is wrong";
+    }
+}
