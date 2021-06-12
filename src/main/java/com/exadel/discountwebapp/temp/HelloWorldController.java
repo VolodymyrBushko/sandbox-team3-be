e0080b70c12ca58ad7978a28abafba9a6814f01c
dchernov <dchernov@exadel.com>
2021-06-12 22:04:08 +0300

    Test commit

--- src/main/java/com/exadel/discountwebapp/temp/HelloWorldController.java
+++ src/main/java/com/exadel/discountwebapp/temp/HelloWorldController.java
@@ -1,13 +1,14 @@
 package com.exadel.discountwebapp.temp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController
 {
-    @GetMapping
-    public String hello()
+    @GetMapping( "/{name}" )
+    public String hello( @PathVariable( "name" ) String name )
     {
         return "Hello world! This is Sandbox";
     }
