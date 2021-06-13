aac3ff2a321d442971d5a9b5215e476cdb43d489
hreshchuk_oleksandr <hreshchuk@gmail.com>
2021-06-13 22:14:24 +0300

    Vendor Repository. Find By Title Optional

--- src/main/java/com/exadel/discountwebapp/vendor/repository/VendorRepository.java
+++ src/main/java/com/exadel/discountwebapp/vendor/repository/VendorRepository.java
@@ -4,6 +4,9 @@ import com.exadel.discountwebapp.vendor.entity.Vendor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VendorRepository extends CrudRepository<Vendor, Long> {
+    Optional<Vendor> findByTitle(String title);
 }
