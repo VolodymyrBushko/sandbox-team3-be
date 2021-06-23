e2a55938d32e5761702eb74e898ae44418353d54
Bozhokmaria <bozhokmaria@gmail.com>
2021-06-23 13:02:29 +0300

    adding tags to liquibase

---
+++ src/main/resources/db/changelog/init/sql/insert_data_tags.sql
@@ -0,0 +1,17 @@
+INSERT INTO tag (tag_id, tag_name, tag_created, tag_modified, cat_id)
+VALUES (1, 'whey protein', current_timestamp, current_timestamp, 1);
+
+INSERT INTO tag (tag_id, tag_name, tag_created, tag_modified, cat_id)
+VALUES (2, 'gym', current_timestamp, current_timestamp, 1);
+
+INSERT INTO tag (tag_id, tag_name, tag_created, tag_modified, cat_id)
+VALUES (3, 'cinema', current_timestamp, current_timestamp, 2);
+
+INSERT INTO tag (tag_id, tag_name, tag_created, tag_modified, cat_id)
+VALUES (4, 'restaurant', current_timestamp, current_timestamp, 2);
+
+INSERT INTO tag (tag_id, tag_name, tag_created, tag_modified, cat_id)
+VALUES (5, 'sea', current_timestamp, current_timestamp, 3);
+
+INSERT INTO tag (tag_id, tag_name, tag_created, tag_modified, cat_id)
+VALUES (6, 'tour guide', current_timestamp, current_timestamp, 3);
---
+++ src/test/java/com/exadel/discountwebapp/tag/service/TagServiceIntegrationTest.java
@@ -0,0 +1,82 @@
+package com.exadel.discountwebapp.tag.service;
+
+import com.exadel.discountwebapp.tag.repository.TagRepository;
+import com.exadel.discountwebapp.tag.vo.TagRequestVO;
+import com.google.common.collect.Lists;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:sql/tag-init.sql")
+@Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:sql/clean-up.sql")
+public class TagServiceIntegrationTest {
+    @Autowired
+    private TagService tagService;
+    @Autowired
+    private TagRepository tagRepository;
+
+
+    @Test
+    void shouldGetAllTags() {
+        var expectedItr = tagRepository.findAll();
+        var expected = Lists.newArrayList(expectedItr);
+        var actual = tagService.findAll();
+
+        assertEquals(4, actual.size());
+
+        assertEquals(expected.get(0).getId(), actual.get(0).getId());
+        assertEquals(expected.get(0).getName(), actual.get(0).getName());
+
+        assertEquals(expected.get(1).getId(), actual.get(1).getId());
+        assertEquals(expected.get(1).getName(), actual.get(1).getName());
+
+        assertEquals(expected.get(2).getId(), actual.get(2).getId());
+        assertEquals(expected.get(2).getName(), actual.get(2).getName());
+
+        assertEquals(expected.get(3).getId(), actual.get(3).getId());
+        assertEquals(expected.get(3).getName(), actual.get(3).getName());
+    }
+
+    @Test
+    void shouldGetTagById() {
+        var id = 1L;
+        var expected = tagRepository.findById(id).get();
+        var actual = tagService.findById(id);
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getName(), actual.getName());
+    }
+
+    @Test
+    void shouldCreateTag() {
+        var expected = TagRequestVO.builder().name("cats").categoryId(10L).build();
+        var createTag = tagService.create(expected);
+
+        var actual = tagRepository.findById(createTag.getId()).get();
+
+        assertNotNull(actual);
+        assertNotNull(actual.getId());
+
+        assertEquals(expected.getName(), actual.getName());
+        assertEquals(expected.getCategoryId(), actual.getCategory().getId());
+    }
+
+    @Test
+    void shouldUpdateTagById() {
+        var id = 1L;
+        var expected = TagRequestVO.builder().name("dogs").categoryId(20L).build();
+        var updateTag = tagService.update(id, expected);
+
+        var actual = tagRepository.findById(updateTag.getId()).get();
+
+        assertNotNull(actual);
+        assertEquals(actual.getId(), id);
+
+        assertEquals(expected.getName(), actual.getName());
+        assertEquals(expected.getCategoryId(), actual.getCategory().getId());
+    }
+}
