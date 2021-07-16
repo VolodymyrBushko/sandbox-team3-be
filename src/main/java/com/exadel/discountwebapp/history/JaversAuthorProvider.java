7a38d96e42447dd9e3fbdd671de56303a4519c1a
hreshchuk_oleksandr <hreshchuk@gmail.com>
2021-07-16 10:49:20 +0300

    Fix security - get current user name
    Add differences Changes

--- src/main/java/com/exadel/discountwebapp/history/JaversAuthorProvider.java
+++ src/main/java/com/exadel/discountwebapp/history/JaversAuthorProvider.java
@@ -1,22 +1,14 @@
 package com.exadel.discountwebapp.history;
 
-import com.exadel.discountwebapp.security.JwtProvider;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-import javax.security.auth.Subject;
-import java.nio.file.Path;
-import java.security.Principal;
-
 @Component
 @NoArgsConstructor
-@AllArgsConstructor
 @Primary
 class JaversAuthorProvider implements org.javers.spring.auditable.AuthorProvider {
 
--- src/main/java/com/exadel/discountwebapp/history/controller/HistoryController.java
+++ src/main/java/com/exadel/discountwebapp/history/controller/HistoryController.java
@@ -3,9 +3,10 @@ package com.exadel.discountwebapp.history.controller;
 import com.exadel.discountwebapp.history.entityclassenum.HistoryEnum;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.apache.commons.lang3.EnumUtils;
+import org.javers.core.Changes;
 import org.javers.core.Javers;
 import org.javers.core.metamodel.object.CdoSnapshot;
+import org.javers.repository.jql.JqlQuery;
 import org.javers.repository.jql.QueryBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,15 +23,41 @@ public class HistoryController {
 
     @SneakyThrows
     @GetMapping("/{inputClazz}/{id}")
-    public String getHistory(@PathVariable HistoryEnum inputClazz,
-                             @PathVariable Long id) {
+    public String getHistoryByEntityId(@PathVariable HistoryEnum inputClazz,
+                                       @PathVariable Long id) {
 
         var clazz = Class.forName(inputClazz.getClazz);
 
-        var jqlQuery = QueryBuilder.byClass(clazz);
+        var jqlQuery = QueryBuilder.byInstanceId(id, clazz);
 
         List<CdoSnapshot> snapshots = javers.findSnapshots(jqlQuery.build());
 
         return javers.getJsonConverter().toJson(snapshots);
     }
+
+    @SneakyThrows
+    @GetMapping("/diff/{inputClazz}/{id}")
+    public String getDifferenceByEntityId(@PathVariable HistoryEnum inputClazz,
+                             @PathVariable Long id) {
+
+        var clazz = Class.forName(inputClazz.getClazz);
+
+        var query = QueryBuilder.byInstanceId(id, clazz).build();
+
+        var changes = javers.findChanges(query);
+
+
+        return javers.getJsonConverter().toJson(changes);
+    }
+
+    @SneakyThrows
+    @GetMapping("/general")
+    public String getDifferenceByEntityId() {
+
+        var changes = javers.findChanges( QueryBuilder.anyDomainObject().build());
+
+        return javers.getJsonConverter().toJson(changes);
+    }
+
+
 }
