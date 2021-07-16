93468d44916f0a906a0dbc22dc6602e4ca22dd6c
hreshchuk_oleksandr <hreshchuk@gmail.com>
2021-07-16 09:05:58 +0300

    Fix security - get current user name

--- src/main/java/com/exadel/discountwebapp/history/JaversAuthorProvider.java
+++ src/main/java/com/exadel/discountwebapp/history/JaversAuthorProvider.java
@@ -1,12 +1,16 @@
 package com.exadel.discountwebapp.history;
 
+import com.exadel.discountwebapp.security.JwtProvider;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import javax.security.auth.Subject;
 import java.nio.file.Path;
 import java.security.Principal;
 
@@ -15,11 +19,10 @@ import java.security.Principal;
 @AllArgsConstructor
 @Primary
 class JaversAuthorProvider implements org.javers.spring.auditable.AuthorProvider {
-    private Principal principal;
 
     @Override
     public String provide() {
-        return principal.getName();
+        return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 
     @Bean
