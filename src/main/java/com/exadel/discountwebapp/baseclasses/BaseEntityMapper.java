ffb9daa7ea60bdea1dd1a34f85986225d100c2a0
Bozhokmaria <bozhokmaria@gmail.com>
2021-07-21 19:17:07 +0300

    cosmetic changes

--- src/main/java/com/exadel/discountwebapp/baseclasses/BaseEntityMapper.java
+++ src/main/java/com/exadel/discountwebapp/baseclasses/BaseEntityMapper.java
@@ -1,7 +1,5 @@
 package com.exadel.discountwebapp.baseclasses;
 
-import org.springframework.stereotype.Component;
-
 public interface BaseEntityMapper<ENTITY, RESPONSE_VO> {
     RESPONSE_VO toVO(ENTITY entity);
 }
